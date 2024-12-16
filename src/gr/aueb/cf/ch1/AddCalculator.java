88886f1cde6ab4a32aebf77a88d1caef368c989e
Philip Dimitriadis <philipdimitriadis@gmail.com>
2024-12-16 19:42:35 +0200

    project calculator created

---
+++ src/gr/aueb/cf/ch1/AddCalculator.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf.ch1;
+
+import java.util.Scanner;
+
+public class AddCalculator {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int result = 0;
+
+        result = add(num1, num2);
+
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
